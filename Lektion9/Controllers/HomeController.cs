a01e61d71c6b6a03d19f045798503d27d35cbc53
David Gullmarsvik <awt2.2012@gmail.com>
2012-09-06 10:50:28 +0200

    Uppgift 2

--- Lektion9/Controllers/HomeController.cs
+++ Lektion9/Controllers/HomeController.cs
@@ -4,19 +4,30 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Lektion9.Models.Entities;
+using Lektion9.Models.Repositories.Abstract;
+using Lektion9.Models.Repositories;
 
 namespace Lektion9.Controllers
 {
     public class HomeController : Controller
     {
+        private IProductRepository Repo;
+
+        public HomeController(IProductRepository repo) {
+            Repo = repo;
+        }
+
         public ActionResult Index()
         {
             ViewBag.Message = "Welcome to ASP.NET MVC!";
 
             Product prod = new Product();
             prod.Name = "Dummy Product - No Repository Interaction";
+            prod.Price = 35;
+            Repo.Add(prod);
+            var prod2 = Repo.Products.FirstOrDefault();
 
-            return View(prod);
+            return View(prod2);
         }
 
         public ActionResult About()
