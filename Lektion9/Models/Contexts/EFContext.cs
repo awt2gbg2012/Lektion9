a04146e878229d7499f779c8254b7e16c5fdc959
David Gullmarsvik <awt2.2012@gmail.com>
2012-09-06 10:59:56 +0200

    Uppgift 10

--- Lektion9/Models/Contexts/EFContext.cs
+++ Lektion9/Models/Contexts/EFContext.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using Lektion9.Models.Entities;
 
 namespace Lektion9.Models.Contexts
 {
     public class EFContext : DbContext
     {
-
+        public DbSet<Product> Products { get; set; }
     }
 }
