0fa85939ff51e4729db547b5f768155ccf30485c
David Gullmarsvik <awt2.2012@gmail.com>
2012-09-06 11:05:45 +0200

    Uppgift 11

--- Lektion9/Models/Repositories/ProductRepository.cs
+++ Lektion9/Models/Repositories/ProductRepository.cs
@@ -4,17 +4,19 @@ using System.Linq;
 using System.Web;
 using Lektion9.Models.Repositories.Abstract;
 using Lektion9.Models.Entities;
+using Lektion9.Models.Contexts;
+using System.Data;
 
 namespace Lektion9.Models.Repositories
 {
     public class ProductRepository : IProductRepository
     {
-        private List<Product> _dbcontext = new List<Product>();
+        private EFContext _dbcontext = new EFContext();
         
-        public IQueryable<Product> Products { get { return _dbcontext.AsQueryable<Product>(); /* return _dbcontext.Products; */ } }
-        public void Add(Product p) { _dbcontext.Add(p); /* _dbcontext.Add(p); _dbcontext.SaveChanges(); */ }
-        public void Edit(Product p) { _dbcontext.Insert(_dbcontext.IndexOf(p), p); /* _db.Entry(entity).State = EntityState.Modified; _dbcontext.SaveChanges(); */ }
-        public void Remove(Product p) { _dbcontext.Remove(p); /* _db.Entry(entity).State = EntityState.Deleted; _dbcontext.SaveChanges(); */ }
+        public IQueryable<Product> Products { get { return _dbcontext.Products; } }
+        public void Add(Product p) { _dbcontext.Products.Add(p); _dbcontext.SaveChanges(); }
+        public void Edit(Product p) { _dbcontext.Entry(p).State = EntityState.Modified; _dbcontext.SaveChanges(); }
+        public void Remove(Product p) { _dbcontext.Entry(p).State = EntityState.Deleted; _dbcontext.SaveChanges(); }
         public Product Get(Guid id) { return Products.Where(p => p.Id == id).FirstOrDefault(); }
     }
 }
